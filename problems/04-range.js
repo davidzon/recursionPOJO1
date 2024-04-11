39f84215b0e3997eb085f8699e2c5873bc550a58
robweekes <weekes.robert@gmail.com>
2024-04-11 15:53:06 -0700

    done with 4

--- problems/04-range.js
+++ problems/04-range.js
@@ -10,19 +10,29 @@ range(3, 4); // [3]
 range(7, 6); // []
 ***********************************************************************/
 
+let newArr = [];
 
 function range(start, end) {
     if (end < start) {
         return [];
-    } else if() {
-      return range(start, end);
+    } else if(start === end) {
+        let result = newArr;        // copy the array to print the result
+        newArr = [];               // clear the constructed array for the next function call on line 32
+        return result;
+
+    } else
+    if(start !== end) {
+        newArr.push(start);
+        console.log(newArr);
+        return range(start + 1, end);
     }
 }
-
 
 console.log(range(1, 5)); // [1, 2, 3, 4]
 console.log(range(3, 4)); // [3]
 console.log(range(7, 6)); // []
+
+console.log(range(1, 100)); // [1, 2, 3, 4]
 
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
